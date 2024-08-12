b0fe6ada96ad94a717d751fa678e55b2d6357111
Nikita Uzakov <nikitauzakov@gmail.com>
2024-08-13 01:08:11 +0400

    master: added server build

---
+++ server/dist/constants/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Errors = void 0;
+var Errors;
+(function (Errors) {
+    Errors["AdNotFound"] = "Ad not found";
+    Errors["NotFound"] = "Not found";
+})(Errors || (exports.Errors = Errors = {}));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9jb25zdGFudHMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsSUFBWSxNQUdYO0FBSEQsV0FBWSxNQUFNO0lBQ2hCLHFDQUEyQixDQUFBO0lBQzNCLGdDQUFzQixDQUFBO0FBQ3hCLENBQUMsRUFIVyxNQUFNLHNCQUFOLE1BQU0sUUFHakIifQ==
